afce25a20e6923e98370704555327bcf84412d70
bodowd <odowd.bing@gmail.com>
2023-11-17 10:20:26 +0100

    Add some notes from chapter 18

--- value.h
+++ value.h
@@ -25,8 +25,14 @@ typedef enum {
 // so with 8-byte double + 1 byte bool, the union will be 8-bytes total, but
 // reuse the first byte for example if it is a bool (vs 9byte if it were a
 // struct)
+
+// on a 64 bit machine with a typical C compiler, the layout looks like this:
+// ----  ----     --------
+// type  padding  as (boolean 1 byte, or number 8 bytes)
 typedef struct {
+  // type tag -- type of the value
   ValueType type;
+  // value so far can be a bool or double
   union {
     bool boolean;
     double number;
@@ -40,12 +46,21 @@ typedef struct {
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
-#define AS_BOOL(value) ((value).as.boolean)
-#define AS_NUMBER(value) ((value).as.number)
-
+// takes a C value of the appropriate type and produces a Value that has the
+// correct type tag and contains the underlying value
+// I think this part of code:
+// (Value){VAL_BOOL, {.boolean = value}}
+// takes the Value struct, and then sets the type field to VAL_BOOL
+// and it sets the boolean field of the union `as` and sets it to
+// the value
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+// to do anything with a Value, we need to unpack it and get the C value back
+// returns true if the Value has that type
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
 
 // constant pool is a dynamic array of values
 typedef struct {
